bd67a9c2bfde9b70c2010c4c3c2309bba31c06a3
Tamosa <77878238+TamosaMondal@users.noreply.github.com>
2023-08-03 00:00:45 +0530

    Create printing numbers from 0 to 3.java

---
+++ printing numbers from 0 to 3.java
@@ -0,0 +1,19 @@
+/* Recursively printing numbers from 0 to 3:---->
+*/
+class Recursion {
+    static int cnt = 0;
+    static void print(){
+        
+         // Base Condition.
+         if(cnt == 3 ) return;
+         System.out.println(cnt);
+
+         // Count incremented.
+         cnt = cnt+1;
+         print();
+
+    }
+    public static void main(String[] args) {
+        print();
+    }
+}
