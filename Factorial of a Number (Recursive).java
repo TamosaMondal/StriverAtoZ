7c44a7b27f657966bc67805dd5e18276b8cb2017
Tamosa <77878238+TamosaMondal@users.noreply.github.com>
2023-08-03 23:51:45 +0530

    Not done code studio

--- Factorial of a Number (Recursive).java
+++ Factorial of a Number (Recursive).java
@@ -2,6 +2,7 @@
 To obtain the factorial of a number, it has to be multiplied by all the whole numbers preceding it. More precisely X! = X*(X-1)*(X-2) … 1.
 Note: X  is always a positive number. 
 */
+
 class Recursion {
     static int factorial(int n){
             if(n == 0){
