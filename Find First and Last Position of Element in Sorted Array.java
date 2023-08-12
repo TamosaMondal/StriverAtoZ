926f65bda31b59f106fdc8d74a20b62ec45787d7
Tamosa <77878238+TamosaMondal@users.noreply.github.com>
2023-08-12 12:21:37 +0530

    🔥✔Without binary Search||Java

---
+++ Find First and Last Position of Element in Sorted Array.java
@@ -0,0 +1,31 @@
+/*Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+If target is not found in the array, return [-1, -1].
+
+You must write an algorithm with O(log n) runtime complexity.
+*/
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int n=nums.length;
+        int first=-1;
+        int last=-1;
+        for(int i=0;i<n;i++){
+            if(nums[i]==target){
+                first=i;
+                break;
+            }
+        }
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]==target){
+                last=i;
+                break;
+            }
+        }
+        int[] temp=new int[2];
+        for(int i=0;i<temp.length-1;i++){
+            temp[i]=first;
+            temp[i+1]=last;
+        }
+        return temp;
+    }
+}
